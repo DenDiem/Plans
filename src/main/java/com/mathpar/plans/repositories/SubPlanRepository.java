4d7eac0608788cd7833a6b2e45049124a621bb78
maksim <ryepkin.maksym98@gmail.com>
2021-06-07 00:02:52 +0300

    db classes for sub plan

---
+++ src/main/java/com/mathpar/plans/repositories/SubPlanRepository.java
@@ -0,0 +1,7 @@
+package com.mathpar.plans.repositories;
+
+import com.mathpar.plans.entities.SubPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubPlanRepository extends JpaRepository<SubPlan, Long> {
+}
