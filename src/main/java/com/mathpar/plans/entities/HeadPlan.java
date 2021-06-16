f02e0b6223d0efbb7097c43227c3064f8d1bf3d1
Vasylenko Oleksandr <olexandr.vasylenko257@gmail.com>
2021-06-16 19:37:06 +0300

    Added javadoc

--- src/main/java/com/mathpar/plans/entities/HeadPlan.java
+++ src/main/java/com/mathpar/plans/entities/HeadPlan.java
@@ -11,39 +11,70 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Клас сутності учбового плану
+ * @author Василенко Олександр
+ */
 @Data
 @NoArgsConstructor
 @Entity(name = "head_plans_table")
 public class HeadPlan implements Serializable {
+    /**
+     * Унікальний ідентифікатор
+     */
     @Id
     @Column(name = "head_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long head_id;
 
+    /**
+     * Назва учбового плану
+     */
     @Column(name = "name")
     private String name;
 
+    /**
+     * Дата і час початку
+     */
     @Column(name = "start_date")
     @Temporal(TemporalType.TIMESTAMP)
     private Date startDate;
 
+    /**
+     * Дата і час завершення
+     */
     @Column(name = "end_date")
     @Temporal(TemporalType.TIMESTAMP)
     private Date endDate;
 
+    /**
+     * Дата і час створення плану
+     */
     @CreationTimestamp
     @Column(name = "creation_date")
     @Temporal(TemporalType.TIMESTAMP)
     private Date creationDate;
 
+    /**
+     * Дата і час останнього оновлення плану
+     */
     @UpdateTimestamp
     @Column(name = "update_date")
     @Temporal(TemporalType.TIMESTAMP)
     private Date updateDate;
 
+    /**
+     * Зв'язок один до багатьох з пунктами плану
+     */
     @OneToMany(mappedBy = "head_plan_var", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<SubPlan> subPlans_vars;
 
+    /**
+     * Конструктор для створення учбового плану
+     * @param name Назва учбового плану
+     * @param startDate Дата і час початку
+     * @param endDate Дата і час завершення
+     */
     public HeadPlan(String name, Date startDate, Date endDate) {
         this.name = name;
         this.startDate = startDate;
@@ -51,6 +82,13 @@ public class HeadPlan implements Serializable {
         this.subPlans_vars = new ArrayList<>();
     }
 
+    /**
+     * Конструктор для створення учбового плану з списком пунктів
+     * @param name Назва учбового плану
+     * @param startDate Дата і час початку
+     * @param endDate Дата і час завершення
+     * @param subPlans список пунктів плану
+     */
     public HeadPlan(String name, Date startDate, Date endDate, List<SubPlan> subPlans){
         this.name = name;
         this.startDate = startDate;
--- src/main/java/com/mathpar/plans/services/SubPlanService.java
+++ src/main/java/com/mathpar/plans/services/SubPlanService.java
@@ -12,26 +12,53 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.Optional;
 
+/**
+ * Сервіс для здійснення операцій над підпунктами учбових планів, використовуючи доступ до БД через JPA Repository
+ * @author Максим Рєпкін
+ */
 @Service
 public class SubPlanService {
 
     @Autowired
     private SubPlanRepository subPlanRepository;
 
+    /**
+     * Метод пошуку пункту учбового палану за унікальним ідентифікатором
+     * @param subPlanId id шуканого пункту учбового плану
+     * @return об'єкт SubPlan, якщо пункт знайдено у БД, інакше - null
+     */
     public SubPlan getSubPlanById(long subPlanId){
         return subPlanRepository.getById(subPlanId);
     }
 
+    /**
+     * Метод створення нового пункту учбового плану в БД
+     * @param name назва пункту
+     * @param type тип пункту (SubPlanType: Lecture, Practice, Exam)
+     * @param order порядковий номер в межах плану
+     * @param headPlan учбовий план, якому належить
+     * @param timeToComplete час, відведений на виконання
+     * @return об'єкт SubPlan, що відповідає новому запису у БД
+     */
     public SubPlan createSubPlan(String name, SubPlanType type, int order, HeadPlan headPlan, Optional<Integer> timeToComplete){
         SubPlan newSubPlan = new SubPlan(name, type, order, headPlan);
         timeToComplete.ifPresent(newSubPlan::setTimeToComplete);
         return subPlanRepository.saveAndFlush(newSubPlan);
     }
 
+    /**
+     * Метод оновлення пункту учбового плану в БД. Якщо пункту з вказаним id не існує, створюється новий запис у БД
+     * @param subPlan об'єкт SubPlan що відповідає запису у БД, який треба оновити
+     * @return об'єкт SubPlan, що відповідає оновленому/новому запису у БД
+     */
     public SubPlan updateSubPlan(SubPlan subPlan){
         return subPlanRepository.save(subPlan);
     }
 
+    /**
+     * Метод видалення пункту учбового плану в БД
+     * @param headPlanId id шуканого пункту
+     */
     @Transactional
     public void purgeSubPlanById(long headPlanId){
         subPlanRepository.deleteById(headPlanId);
